ca968769a000185327f0530cef63677821373ec4
leonardo-vargas-de-paula <leonardovargasdpaula@gmail.com>
2026-01-01 12:57:35 -0400

    feat: adiciona parking

--- parking/admin.py
+++ parking/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import ParkingSpot, ParkingRecord
 # Register your models here.
+
+admin.site.register(ParkingSpot)
+admin.site.register(ParkingRecord)
