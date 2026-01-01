fd2dc8fbcc463d55e3e485263ede4ff545ea3442
leonardo-vargas-de-paula <leonardovargasdpaula@gmail.com>
2026-01-01 15:23:26 -0400

    feat: adiciona drf

--- core/settings.py
+++ core/settings.py
@@ -36,12 +36,16 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'jazzmin',
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'rest_framework',
+    
     'customers',
     'vehicles',
     'parking',
