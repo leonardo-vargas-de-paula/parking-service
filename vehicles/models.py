eae3c49e920ff356c8eb6c7ab88aa48e7fa1f5e4
leonardo-vargas-de-paula <leonardovargasdpaula@gmail.com>
2026-01-01 11:40:00 -0400

    feat: add vehicles

---
+++ vehicles/models.py
@@ -0,0 +1,76 @@
+from django.db import models
+
+from customers.models import Customer
+
+class VehicleType(models.Model):
+    name = models.CharField(
+        max_length=50, verbose_name='Tipo de Veículo',
+        unique=True,
+        verbose_name='Nome'
+    )
+    description = models.TextField(
+        blank=True,
+        null=True,
+        verbose_name='Descrição'    
+    )
+    
+    def __str__(self):  
+        return self.name
+    
+    class Meta:
+        verbose_name = 'Tipo de Veículo'
+        verbose_name_plural = 'Tipos de Veículos'
+    
+class Vehicle(models.Model):
+    vehicle_type = models.ForeignKey(
+        VehicleType,
+        on_delete=models.PROTECT,
+        blank=True,
+        null=True,
+        related_name='vehicles',
+        verbose_name='Tipo de Veículo'
+    )
+    
+    owner = models.ForeignKey(
+        Customer,
+        on_delete=models.PROTECT,
+        blank=True,
+        null=True,
+        related_name='vehicles',
+        verbose_name='Proprietário'        
+    )
+    
+    license_plate = models.CharField(
+        max_length=11,
+        unique=True,
+        verbose_name='Placa'
+    )
+    brand = models.CharField(
+        max_length=50,
+        blank=True,
+        null=True,
+        verbose_name='Marca'
+    )
+    model = models.CharField(
+        max_length=50,
+        blank=True,
+        null=True,
+        verbose_name='Modelo'
+    )
+    color = models.CharField(
+        max_length=50,
+        blank=True,
+        null=True,
+        verbose_name='Cor'
+    )
+    
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Criado em')      
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Atualizado em')    
+    
+    
+    def __str__(self):  
+        return self.license_plate
+    
+    class Meta:
+        verbose_name = 'Veículo'
+        verbose_name_plural = 'Veículos'    
