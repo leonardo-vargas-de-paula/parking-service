4d0bb74458e8b861d84a265612417360df0f9d58
leonardo-vargas-de-paula <leonardovargasdpaula@gmail.com>
2026-01-02 10:06:06 -0400

    fix: corrige vehicle na queryset de vehicleviewset

--- vehicles/views.py
+++ vehicles/views.py
@@ -2,24 +2,27 @@ from rest_framework.permissions import IsAdminUser, DjangoModelPermissions
 from core.permissions import IsOwnerOfVehicleOrRecord
 
 from rest_framework import viewsets
+from vehicles.filters import VehicleFilterClass, VehicleTypeFilterClass
 from vehicles.models import Vehicle, VehicleType
 from vehicles.serializers import VehicleSerializer, VehicleTypeSerializer
 
 class VehicleViewSet(viewsets.ModelViewSet):
     queryset = Vehicle.objects.all()
     serializer_class = VehicleSerializer
+    rql_filter_class = VehicleFilterClass   
     permission_classes = [DjangoModelPermissions, IsOwnerOfVehicleOrRecord]
     
     def get_queryset(self):
         user = self.request.user
         
         if user.is_staff:
-            return VehicleType.objects.all()
+            return Vehicle.objects.all()
         return Vehicle.objects.filter(owner__user=user)
     
 class VehicleTypeViewSet(viewsets.ModelViewSet):
     queryset = VehicleType.objects.all()
     serializer_class = VehicleTypeSerializer
+    rql_filter_class = VehicleTypeFilterClass
     permission_classes = [DjangoModelPermissions, IsOwnerOfVehicleOrRecord]
     
     def get_queryset(self):
