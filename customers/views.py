c1f54a25bbb48f90ce390ce70f3dde2f54cd1461
leonardo-vargas-de-paula <leonardovargasdpaula@gmail.com>
2026-01-02 09:39:42 -0400

    feat: add filtros em customers

--- customers/views.py
+++ customers/views.py
@@ -2,9 +2,12 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAdminUser, DjangoModelPermissions
 from customers.models import Customer
 from customers.serializers import CustomerSerializer
+from customers.filters import CustomerFilterClass
+
 
 #findall - get all customers
 class CustomerViewSet(viewsets.ModelViewSet):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
+    rql_filter_class = CustomerFilterClass
     list_permission_classes = [DjangoModelPermissions, IsAdminUser]
