ad772cc998183ee762ee90d9ac5d37ba9b4b3759
leonardo-vargas-de-paula <leonardovargasdpaula@gmail.com>
2026-01-02 09:39:33 -0400

    feat: add django rql

--- core/settings.py
+++ core/settings.py
@@ -279,5 +279,9 @@ REST_FRAMEWORK = {
 
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
+    ],
+    
+    'DEFAULT_FILTER_BACKENDS': [
+        'dj_rql.drf.RQLFilterBackend'
     ]
 }
